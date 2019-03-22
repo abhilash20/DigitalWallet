6069910707942a15bd7902efec6a109645e29f31
abhilash yayi <abhilash.y20@gmail.com>
2019-03-21 21:24:29 -0400

    clean

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ let port = 3000;
 
 mongoose.connect(process.env.DB_URI);
 app.use(morgan('dev'));
-// app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use('/rest/v1/users', usersRouter);
 
